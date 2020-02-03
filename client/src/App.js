de7b075e5a3081a8da6ca985fb359f7669de4dad
jonathanzea <jonathanzea91@gmail.com>
2020-02-03 10:19:40 +0100

    update header

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Fib Calculator (v 2.0)</h1>
+          <h1 className="App-title"> Fib Calculator & Kubernetes (v 2.0)</h1>
           <Link to="/">Home</Link>
           <Link to="/otherPage">Other Page</Link>
         </header>
